2a1195bb5a04c6c6c04ce6af944d6d48d3d0ddaf
root <sylyac2000@gmail.com>
2022-10-19 22:34:09 -0700

    Using what you did in the task #0, extend your Python script to export data in the CSV format.

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,30 +5,23 @@ returns information about his/her TODO list progress
 """
 
 
+import csv
+import requests
+from sys import argv
+
 if __name__ == '__main__':
-    import csv
-    import requests
-    from sys import argv
+    if len(argv) > 1:
+        theid = int(argv[1])
+        API_URL = "https://jsonplaceholder.typicode.com/"
+        user = requests.get("{}users/{}".format(API_URL, theid)).json()
+        name = user.get("name")
+        if name is not None:
+            todos = requests.get(
+                "{}todos?userId={}".format(
+                    API_URL, theid)).json()
 
-    if len(argv) == 2:
-        id = int(argv[1])
-
-        API_URL = "https://jsonplaceholder.typicode.com"
-
-        """ get user """
-        response = requests.get('{}/users/{}'.format(API_URL, id))
-        user = response.json()
-
-        """ get user's todos """
-        response = requests.get('{}/users/{}/todos'.format(API_URL, id))
-        todos = response.json()
-        emp_name = user.get('name')
-        tasks = list(filter(lambda x: x.get('userId') == id, todos))
-        completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
-        username = user.get("username")
-
-        with open("{}.csv".format(id), "w", newline="") as csvfile:
-            writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-            [writer.writerow(
-                [id, username, t.get("completed"), t.get("title")]
-            ) for t in tasks]
+            with open("{}.csv".format(theid), 'w', newline='') as csvfile:
+                taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+                for t in todos:
+                    taskwriter.writerow([theid, user.get('username'),
+                                        t.get('completed'), t.get('title')])
